b07c37060cd6a326a5380d2f05b8d8113d9d801e
MonarhInQuisitor <tsapdmytro1996@gmail.com>
2024-10-09 13:54:28 +0300

    Update FP.js

--- 1/1.1/FP.js
+++ 1/1.1/FP.js
@@ -4,26 +4,30 @@ const TEXT = `44.38,34.33,Алушта,3140040,\n49.46,30.17,Біла Церкв
 49.54,28.49,Бердичйййів,575\n49.54,28.49,Берд,8475,
 44.38,34.33,Алуштавввввв,314403000,\n49.46,30.17,Білаяп Церква,299001,
 44.38,34.33,Алуштавввв,31\n49.46,30.17,Білаяпк Церква,29900,\n #некоммен`
+/**
+ * 
+ * @param {String} CSV text
+ * @returns function that replace cities on new text
+ */
 function first(CSV) {
     let regExp = /^\d+\.\d+,\d+\.\d+,[\wа-яА-ЯіІїЇєЄ ]+,\d+,$/;
     let cities = CSV.split(/\n/).filter(el => el.match(regExp)).map(el => {
         let sub = el.split(",")
-        return { x: sub.shift(), y: sub.shift(), name: sub.shift(), population: sub.shift() }
+        return { x: sub.shift(), y: sub.shift(), name: sub.shift(), population: sub.shift() } // sub.shift() also return the element
     }).sort((a, b) => b.population - a.population).slice(0, 9).reduce((sum, curr, index) => {
         let name = curr.name
         sum[name] = { population: curr.population, rating: index + 1 };
-        //   console.log(acc)
         return sum
     }, {});
 console.log(cities)
-    
+     
     return (text) => {
-        let reg = new RegExp(Object.keys(cities).join('|'),"g")
+        let reg = new RegExp(Object.keys(cities).join('|'),"g") //regulat expresssion include names of all cities
         return text.replace(reg, (city) => {
             if (cities[city]) {
                 return `Місто ${city} з населенням ${cities[city].population} осіб і рейтингом ${cities[city].rating}`;
             }
-            // Якщо місто не знайдено в об'єкті cities, повертаємо початкову назву міста
+            //if a city doesn't be found will return just name of city
             return city;
         });
     }
